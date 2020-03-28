2000599900955cdf398d5cf2a8f3d524810038fa
Eduardo <edu.ice7@gmail.com>
2020-03-28 02:38:29 +0000

    Add files via upload

---
+++ resources/[vrp]/vrp_hotkeys/client.lua
@@ -0,0 +1,44 @@
+-- MAIN THREAD
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+		for k,v in pairs(cfg.hotkeys) do
+		  if IsControlJustPressed(v.group, k) or IsDisabledControlJustPressed(v.group, k) and GetLastInputMethod(0) then
+		    v.pressed()
+		  end
+
+		  if IsControlJustReleased(v.group, k) or IsDisabledControlJustReleased(v.group, k) and GetLastInputMethod(0) then
+		    v.released()
+		  end
+		end
+	end
+end)
+
+
+-- THIS IS FOR NO DOC COMA
+Citizen.CreateThread(function() -- coma thread
+  while true do
+    Citizen.Wait(1000)
+    if vRP.isInComa({}) then
+	  if called == 0  then
+	    HKserver.canSkipComa({},function(skipper)
+	      if skipper then
+		    HKserver.docsOnline({},function(docs)
+		      if docs == 0 then
+			    vRP.notify({"~r~Não há ninguém para o reviver.\n~b~use ~g~E~b~ para nascer."})
+			  else
+			    vRP.notify({"~g~Para medicos online.\n~b~Use ~g~E~b~ para chamar uma ambulancia."})
+			  end
+		    end)
+          end
+	    end)
+	  else
+	    called = called - 1
+	  end
+	else
+	  called = 0
+	end
+  end
+end)
+
+
